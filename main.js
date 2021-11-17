5a015ab6abf9669dd803b4c42a6f5d59bb8ce602
Oden Gomez <odenbeckgomez22@winchendon.org>
2021-11-17 12:53:00 -0500

    design journal changes

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
-var canvas = document.getElementById("canva");
-var notepad = canvas.getContext("2d");
-notepad.fillStyle = "DarkRed";
-notepad.fillRect(300, 0, 200, 50);
+function draw() {
+    // canvas definition
+    var canvas = document.getElementById("canva");
+    var notepad = canvas.getContext("2d");
 
-notepad.fillStyle = "DarkRed";
-notepad.fillRect(250, 50, 300, 50);
+    // This is the code for the roof of the house
+    notepad.fillStyle = "DarkRed";
+    notepad.fillRect(300, 0, 200, 50);
 
-notepad.fillStyle = "DarkRed";
-notepad.fillRect(200, 100, 400, 50);
+    notepad.fillStyle = "DarkRed";
+    notepad.fillRect(250, 50, 300, 50);
 
-notepad.fillStyle = "Tan";
-notepad.fillRect(250, 150, 50, 200);
+    notepad.fillStyle = "DarkRed";
+    notepad.fillRect(200, 100, 400, 50); 
 
-notepad.fillStyle = "Tan";
-notepad.fillRect(300, 150, 50, 200);
+    // This is the code for the body of the house
+    notepad.fillStyle = "Tan";
+    notepad.fillRect(250, 150, 50, 200);
 
-notepad.fillStyle = "Tan";
-notepad.fillRect(350, 150, 50, 200);
+    notepad.fillStyle = "Tan";
+    notepad.fillRect(300, 150, 50, 200);
 
-notepad.fillStyle = "Tan";
-notepad.fillRect(400, 150, 50, 200);
+    notepad.fillStyle = "Tan";
+    notepad.fillRect(350, 150, 50, 200);
 
-notepad.fillStyle = "Tan";
-notepad.fillRect(450, 150, 50, 200);
+    notepad.fillStyle = "Tan";
+    notepad.fillRect(400, 150, 50, 200);
 
-notepad.fillStyle = "Tan";
-notepad.fillRect(500, 150, 50, 200);
+    notepad.fillStyle = "Tan";
+    notepad.fillRect(450, 150, 50, 200);
 
-notepad.fillStyle = "black";
-notepad.fillRect(350, 250, 100, 100);
+    notepad.fillStyle = "Tan";
+    notepad.fillRect(500, 150, 50, 200);
 
+    // This is the code for the door of the house
+    notepad.fillStyle = "black";
+    notepad.fillRect(350, 250, 100, 100);
+}
+   draw()
 
 
 
